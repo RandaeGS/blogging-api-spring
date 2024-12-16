52f4a6dda817dd1df278ebb1f92e5e996d137b57
RandaeGS <garciasanchezrd@gmail.com>
2024-12-16 03:14:36 -0400

    create delete and create controllers for articles

--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -2,13 +2,13 @@ package controllers;
 
 import encapsulations.Article;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import repositories.ArticleRepository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("articles")
@@ -20,5 +20,21 @@ public class ArticleController {
     @GetMapping("/")
     public List<Article> getArticles(){
         return articleRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteArticle(@PathVariable UUID id){
+        if (!articleRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        articleRepository.deleteById(id);
+        Map<String,String> response = Map.of("Message", "User succesfully deleted");
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> createArticle(@RequestBody Article article){
+        articleRepository.save(article);
+        return ResponseEntity.ok("Article created succesfully");
     }
 }
