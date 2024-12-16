1b2da97f681373a5ccbfc8460e5873843677cfd7
RandaeGS <garciasanchezrd@gmail.com>
2024-12-16 02:45:18 -0400

    add jpa to author class

--- src/main/java/encapsulations/Author.java
+++ src/main/java/encapsulations/Author.java
@@ -1,17 +1,23 @@
 package encapsulations;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
+@Table(name = "Author")
 @Getter @NoArgsConstructor
 public class Author {
+    @Id
     private UUID id;
+    @Column(name = "firstName", nullable = false)
     private String firstName;
+    @Column(name = "lastName", nullable = false)
     private String lastName;
+    @OneToMany
     private List<Article> articleList;
 
     public Author(UUID id, String firstName, String lastName) {
