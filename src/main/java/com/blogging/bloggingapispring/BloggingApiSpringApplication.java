abae058b1822b0ecdaa965c44316bbbaf724dac7
RandaeGS <garciasanchezrd@gmail.com>
2024-12-16 02:31:39 -0400

    init project

---
+++ src/main/java/com/blogging/bloggingapispring/BloggingApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.blogging.bloggingapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogging/bloggingapispring/BloggingApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogging.bloggingapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
