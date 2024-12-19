a4c2e44a3051ecfa7f7843857fdd7269d83f5194
RandaeGS <garciasanchezrd@gmail.com>
2024-12-18 22:44:27 -0400

    finish get author controllers

--- src/main/java/controllers/AuthorController.java
+++ src/main/java/controllers/AuthorController.java
@@ -1,4 +1,38 @@
 package controllers;
 
+import encapsulations.Author;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import repositories.AuthorRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/authors")
 public class AuthorController {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @GetMapping("/")
+    public List<Author> getAuthors(){
+        return authorRepository.findAll();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAuthor(@PathVariable UUID id){
+        Optional<Author> authorOptional = authorRepository.findById(id);
+        if (authorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Author author = authorOptional.get();
+        return ResponseEntity.ok(author);
+    }
 }
